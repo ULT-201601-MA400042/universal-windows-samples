64b8a50ab753cb0c1af8a0a374289689bf0d2de4
Nicholas Attlee <nicholas.attlee1@tafensw.edu.au>
2016-02-27 10:48:43 +1100

    Added sample app from first session

---
+++ session-01/MyFirstApp/MyFirstApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+        }
+
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                NotifyPropertyChanged("LastName");
+            }
+        }
+
+        private ObservableCollection<string> _people = new ObservableCollection<string>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public ObservableCollection<string> People
+        {
+            get { return _people; }
+        }
+
+        public void AddPerson()
+        {
+            var fullName = String.Format("{0} {1}", FirstName, LastName);
+            People.Add(fullName);
+        }
+    }
+}
